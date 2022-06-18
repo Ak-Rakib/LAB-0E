bcdea489466fbefed9a3760d6c9dcc958e61e250
Ak-Rakib <106004707+Ak-Rakib@users.noreply.github.com>
2022-06-18 21:53:39 +0600

    Add files via upload

---
+++ solve without using loop.c
@@ -0,0 +1,46 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n,m,k,j,l,f,y,z,u,q,sum;
+    double avg;
+    printf("Enter a number: ");
+    scanf("%d\n",&n);
+    
+    printf("Enter a number: ");
+    scanf("%d\n",&m);
+    
+    printf("Enter a number: ");
+    scanf(" %d\n",&k);
+    
+    printf("Enter a number: ");
+    scanf("%d\n",&j);
+    
+    printf("Enter a number: ");
+    scanf("%d\n",&l);
+     
+    printf("Enter a number: "); 
+    scanf("%d\n",&f);
+    
+    printf("Enter a number: ");
+    scanf("%d \n",&y);
+    
+    printf("Enter a number: ");
+    scanf("%d\n",&z);
+    
+    printf("Enter a number: ");
+    scanf("%d\n",&u);
+    
+    printf("Enter a number: ");
+    scanf("%d\n",&q);
+    
+    sum = n+m+k+j+l+f+y+z+u+q;
+    printf(" Addition = %d\n ", sum);
+    
+    avg = (n+m+k+j+l+f+y+z+u+q)/10;
+    printf("Average = %lf",avg);
+    
+    return 0;
+}
+    
+
